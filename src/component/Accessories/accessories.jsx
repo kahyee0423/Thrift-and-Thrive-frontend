807347d16c8641c05dccb1fc7db58f8da7071d7e
Florence Hor <kahyee0423@gmail.com>
2024-12-25 14:13:37 +0800

    Solved 4 pages

--- src/component/Accessories/accessories.jsx
+++ src/component/Accessories/accessories.jsx
@@ -1,22 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './accessories.css'
 import NavigationBar from '../NavigationBar/NavigationBar';
 import Footer from '../Footer/footer';
 import ProductCard from '../ProductCard/productCard';
 import SideBar from '../SideBar/SideBar';
 
-const products = Array(12).fill({
+const products = Array(23).fill({
   image: "https://cdn.builder.io/api/v1/image/assets/904907665fd04df7b56e80ff4b56e284/9ccdd56b6dadb8838db5b38d01ae2d77da2ff92f4582e0c18c4b19697847d7d6?apiKey=904907665fd04df7b56e80ff4b56e284&",
   title: "Text",
   price: "0",
   description: "Body text."
 });
 
+const productsPerPage = 12; 
+
 const accessoriesFilters = [
   { title: 'Category', options: ["Bags", "Shoes", "Watches", "Hats & Caps"] },
   { title: 'Price range', options: ["$0 - $50", "$51 - $100", "$101 - $200"] }
 ];
-const accessories = () => {
+const Accessories = () => {
+  const [currentPage, setCurrentPage] = useState(1);
+    
+      const totalPages = Math.ceil(products.length / productsPerPage);
+      const pageNumbers = Array.from({ length: totalPages }, (_, i) => i + 1);
+      const goToPage = (page) => {
+      setCurrentPage(page)
+    };
+
+    const goToPreviousPage = () => {
+      if (currentPage > 1) {
+          setCurrentPage(currentPage - 1);
+      }
+  };
+  
+  const goToNextPage = () => {
+      if (currentPage < totalPages) {
+          setCurrentPage(currentPage + 1);
+      }
+  };
+  
+    const startIndex = (currentPage - 1) * productsPerPage;
+      const endIndex = startIndex + productsPerPage;
+      const displayedProducts = products.slice(startIndex, endIndex);
+
   return (
     <main className="Page">
     <NavigationBar />
@@ -34,16 +60,27 @@ const accessories = () => {
       <SideBar filters={accessoriesFilters} />
 
         <section className="productsGrid">
-          {products.map((product, index) => (
+          {displayedProducts.map((product, index) => (
             <ProductCard key={index} {...product} />
           ))}
         </section>
       </div>
 
-      <div className="pagination">
-        <img src="https://cdn.builder.io/api/v1/image/assets/904907665fd04df7b56e80ff4b56e284/4f7e95dda55a307aca27925c6d27fd04317fe0e7d999525ef33bacc427211444?apiKey=904907665fd04df7b56e80ff4b56e284&" alt="Previous page" className="paginationIcon" />
-        <img src="https://cdn.builder.io/api/v1/image/assets/904907665fd04df7b56e80ff4b56e284/5a0728a287c40c52aa4fff3691c1876efa4f8bfbc04804cbdae35788467475a4?apiKey=904907665fd04df7b56e80ff4b56e284&" alt="Next page" className="paginationIcon" />
-      </div>
+      <div className="paginationWrapper">
+        <div className="pagination">
+          <img src="https://cdn.builder.io/api/v1/image/assets/904907665fd04df7b56e80ff4b56e284/4f7e95dda55a307aca27925c6d27fd04317fe0e7d999525ef33bacc427211444?apiKey=904907665fd04df7b56e80ff4b56e284&" alt="Previous page" className="paginationIcon" onClick={goToPreviousPage}/>
+          {pageNumbers.map((number) => (
+                  <span
+                    key={number}
+                    className={`pageNumber ${currentPage === number ? "active" : ""}`}
+                    onClick={() => goToPage(number)}
+                  >
+                    {number}
+                  </span>
+              ))}
+          <img src="https://cdn.builder.io/api/v1/image/assets/904907665fd04df7b56e80ff4b56e284/5a0728a287c40c52aa4fff3691c1876efa4f8bfbc04804cbdae35788467475a4?apiKey=904907665fd04df7b56e80ff4b56e284&" alt="Next page" className="paginationIcon" onClick={goToNextPage}/>
+        </div>
+        </div>
     </div>
 
     <Footer />
@@ -51,4 +88,4 @@ const accessories = () => {
   )
 }
 
-export default accessories;
+export default Accessories;
